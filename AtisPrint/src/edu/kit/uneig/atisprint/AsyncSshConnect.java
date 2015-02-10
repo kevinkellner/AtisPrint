84f273302c1362d484f6fc736734d3910673eba4
Kevin Kellner <kellnerkev@gmail.com>
2015-02-10 20:49:14 +0100

    Formatting

--- AtisPrint/src/edu/kit/uneig/atisprint/AsyncSshConnect.java
+++ AtisPrint/src/edu/kit/uneig/atisprint/AsyncSshConnect.java
@@ -21,40 +21,46 @@ public class AsyncSshConnect extends AsyncTask<Object, Void, String> {
 
     @Override
     protected String doInBackground(Object... params) {
-      //Don't try this at home. Seriously. It is THAT bad. Open for suggestions tho.
+        // Don't try this at home. Seriously. It is THAT bad. Open for
+        // suggestions tho.
         String user = (String) params[0];
-        String password = (String) params[1];        
+        String password = (String) params[1];
         String hostname = (String) params[2];
         int port = (int) params[3];
         InputStream fis = (InputStream) params[4];
+        try {
             try {
-                try {
-                        System.out.println("HILFE");
-//                final FileInputStream in = (FileInputStream) getContentResolver().openInputStream(receivedUri);
-                        
-                    copyFileOverSCP(user, password, hostname, port, fis);
-//                executeRemoteSSHCommand(user, password, atisHostname, port);
-                } catch (FileNotFoundException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
-                
+                // final FileInputStream in = (FileInputStream)
+                // getContentResolver().openInputStream(receivedUri);
 
-            } catch (Exception e) {
+                copyFileOverSCP(user, password, hostname, port, fis);
+                // executeRemoteSSHCommand(user, password, atisHostname, port);
+                fis.close();
+            } catch (FileNotFoundException e1) {
                 // TODO Auto-generated catch block
-                e.printStackTrace();
+                e1.printStackTrace();
             }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         return "Done";
     }
+
     @Override
     protected void onPostExecute(String result) {
-        delegate.processFinish(result); //callback to activity that started this async task.
+        delegate.processFinish(result); // callback to activity that started
+                                        // this async task.
     }
 
-    public void copyFileOverSCP(String username, String password, String hostname, int port, InputStream file) throws JSchException, FileNotFoundException, SftpException, InterruptedException {
+    public void copyFileOverSCP(String username, String password,
+            String hostname, int port, InputStream file) throws JSchException,
+            FileNotFoundException, SftpException, InterruptedException {
         JSch jsch = new JSch();
         Session session = null;
-        session = jsch.getSession(username,hostname,22);
+        session = jsch.getSession(username, hostname, 22);
         session.setPassword(password);
         Properties prop = new Properties();
         prop.put("StrictHostKeyChecking", "no");
@@ -64,18 +70,16 @@ public class AsyncSshConnect extends AsyncTask<Object, Void, String> {
         ChannelSftp channel = null;
         channel = (ChannelSftp) session.openChannel("sftp");
         channel.connect();
-        //If you want you can change the directory using the following line.
-//            channel.cd(RemoteDirectoryPath)
-        channel.put(file,"testCopySFTP.txt");
+        // If you want you can change the directory using the following line.
+        // channel.cd(RemoteDirectoryPath)
+        channel.put(file, "testCopySFTP.txt");
         channel.disconnect();
         session.disconnect();
-        
+
     }
-    
-    
-    
-    public String executeRemoteSSHCommand(String user, String password, String hostname, int port)
-            throws Exception {
+
+    public String executeRemoteSSHCommand(String user, String password,
+            String hostname, int port) throws Exception {
         JSch jsch = new JSch();
         Session session = jsch.getSession(user, hostname, port);
         session.setPassword(password);
@@ -88,13 +92,18 @@ public class AsyncSshConnect extends AsyncTask<Object, Void, String> {
         session.connect();
 
         // SSH Channel
-        ChannelExec channelssh = (ChannelExec)
-                session.openChannel("exec");
+        ChannelExec channelssh = (ChannelExec) session.openChannel("exec");
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         channelssh.setOutputStream(baos);
 
         // Execute command
-        channelssh.setCommand("touch testFile.txt"+new Random().nextInt()); //create empty testfile if all goes well
+        channelssh.setCommand("touch testFile.txt" + new Random().nextInt()); // create
+                                                                              // empty
+                                                                              // testfile
+                                                                              // if
+                                                                              // all
+                                                                              // goes
+                                                                              // well
         channelssh.connect();
         channelssh.disconnect();
 
--- AtisPrint/src/edu/kit/uneig/atisprint/MainActivity.java
+++ AtisPrint/src/edu/kit/uneig/atisprint/MainActivity.java
@@ -10,8 +10,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -25,84 +23,91 @@ public class MainActivity extends Activity implements AsyncResponse {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        
+
         // Get intent, action and MIME type
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
-        
-        Toast.makeText(this, "Action:"+action+"\ntype:"+type, Toast.LENGTH_LONG).show();
-        
+
+        Toast.makeText(this, "Action:" + action + "\ntype:" + type,
+                Toast.LENGTH_LONG).show();
+
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if ("application/pdf".equals(type)) {
                 handleAsyncSendPdf(intent); // Handle pdf being sent
-            }else if (type.startsWith("image/")) { 
-//                Toast.makeText(this, intent.toString(), Toast.LENGTH_LONG).show();
+            } else if (type.startsWith("image/")) {
+                // Toast.makeText(this, intent.toString(),
+                // Toast.LENGTH_LONG).show();
                 handleSendPdf(intent);
             }
         } else {
-            
+
             setContentView(R.layout.activity_main);
         }
     }
-    
+
     public void handleAsyncSendPdf(Intent intent) {
         InputStream is = null;
         try {
-          is =  getAssets().open("blatt-13-aufgaben.pdf");
-//          is =  getAssets().open("TestFileCopy.txt");
-      } catch (IOException e2) {
-          // TODO Auto-generated catch block
-          e2.printStackTrace();
-      }
-      
-      final InputStream caInput = new BufferedInputStream(is);
-      
-      AsyncSshConnect ssh = new AsyncSshConnect();
-      ssh.delegate = this; //add reference for callback
-      ssh.execute("s_kkelln", "335BA8637F", "i08fs1.ira.uka.de", 22, caInput);
+            is = getAssets().open("blatt-13-aufgaben.pdf");
+            // is = getAssets().open("TestFileCopy.txt");
+        } catch (IOException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        }
+
+        final InputStream caInput = new BufferedInputStream(is);
+
+        AsyncSshConnect ssh = new AsyncSshConnect();
+        ssh.delegate = this; // add reference for callback
+        ssh.execute("s_kkelln", "335BA8637F", "i08fs1.ira.uka.de", 22, caInput);
     }
-    
-    public void handleSendPdf(Intent intent)  {
-//        Uri receivedUri = (Uri)intent.getParcelableExtra(Intent.EXTRA_STREAM);
-        
-//        String filePath = getRealPathFromURI(this, receivedUri);
-//        Toast.makeText(this, filePath, Toast.LENGTH_LONG).show();
-//        File pdf = new File(filePath);
-//        Toast.makeText(this, String.valueOf(pdf.exists()), Toast.LENGTH_SHORT).show();
-        
+
+    public void handleSendPdf(Intent intent) {
+        // Uri receivedUri =
+        // (Uri)intent.getParcelableExtra(Intent.EXTRA_STREAM);
+
+        // String filePath = getRealPathFromURI(this, receivedUri);
+        // Toast.makeText(this, filePath, Toast.LENGTH_LONG).show();
+        // File pdf = new File(filePath);
+        // Toast.makeText(this, String.valueOf(pdf.exists()),
+        // Toast.LENGTH_SHORT).show();
+
         intent.putExtra("user", "s_kkelln");
         intent.putExtra("password", "335BA8637F");
-        
-        intent.setClass(this, SSHConnect.class);
-        
-        /*Intent sshIntent = new Intent(this, SSHConnect.class);
-        sshIntent.putExtra("user", "s_kkelln");
-        sshIntent.putExtra("password", "335BA8637F");
-        sshIntent.putExtra("uri", receivedUri);
-        sshIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); */
+
+        // intent.setClass(this, SSHConnect.class);
+
+        /*
+         * Intent sshIntent = new Intent(this, SSHConnect.class);
+         * sshIntent.putExtra("user", "s_kkelln");
+         * sshIntent.putExtra("password", "335BA8637F");
+         * sshIntent.putExtra("uri", receivedUri);
+         * sshIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+         */
         Bundle userData = new Bundle();
         userData.putString("user", "s_kkelln");
         userData.putString("password", "335BA8637F");
-        
+
         startActivity(intent, userData);
     }
-    
+
     public String getRealPathFromURI(Context context, Uri contentUri) {
         Cursor cursor = null;
-        try { 
-          String[] proj = { MediaStore.Images.Media.DATA };
-          cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
-          int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-          cursor.moveToFirst();
-          return cursor.getString(column_index);
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri, proj, null,
+                    null, null);
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
         } finally {
-          if (cursor != null) {
-            cursor.close();
-          }
+            if (cursor != null) {
+                cursor.close();
+            }
         }
-      }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -122,15 +127,15 @@ public class MainActivity extends Activity implements AsyncResponse {
         }
         return super.onOptionsItemSelected(item);
     }
-    
+
     public void onClickPrint(View v) {
         handleAsyncSendPdf(new Intent());
-      
+
     }
 
     @Override
     public void processFinish(String output) {
         System.out.println(output);
-        
+
     }
 }
