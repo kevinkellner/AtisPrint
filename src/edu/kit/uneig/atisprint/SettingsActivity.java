3747bb60ffc0999229c177f476f39b836ae21f1c
kevin_kellner <kellnerkev@gmail.com>
2015-06-06 14:34:03 +0200

    Added onclick listener to first listview item

--- src/edu/kit/uneig/atisprint/SettingsActivity.java
+++ src/edu/kit/uneig/atisprint/SettingsActivity.java
@@ -8,10 +8,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.TwoLineListItem;
 import edu.kit.uneig.atisprint.login.ObscuredSharedPreferences;
 import edu.kit.uneig.atisprint.login.SaveLoginActivity;
 
@@ -24,16 +28,20 @@ public class SettingsActivity extends Activity {
 
     protected static int SIGN_IN_REQUEST = 0xFF;
 
+    private final int changeUser = 0;
+    private final int selectPrinter = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        //titles contains all the items in the ListView, subtitles are their subitems
         final String[] titles = new String[] {"Change user", "Select printer"};
         final String[] subtitles = new String[] {getUsername(), getPrinter()};
 
         List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+
         for (int i = 0; i < titles.length; i++) {
             Map<String, String> datum = new HashMap<String, String>(2);
             datum.put("title", titles[i]);
@@ -47,6 +55,7 @@ public class SettingsActivity extends Activity {
                                                     new int[]{android.R.id.text1, android.R.id.text2});
 
         ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(new ListItemListener());
         listView.setAdapter(mAdapter);
     }
 
@@ -58,6 +67,7 @@ public class SettingsActivity extends Activity {
     private String getPrinter() {
         return "PrinterTODO";
     }
+
 
     public void onClickPrint(View v) {
         Intent intent = new Intent(this, SaveLoginActivity.class);
@@ -97,5 +107,20 @@ public class SettingsActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ListItemListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            switch (position) {
+                case changeUser:
+                    Intent signIn = new Intent(getBaseContext(), SaveLoginActivity.class);
+                    startActivity(signIn);
+                    break;
+                case selectPrinter:
+                    //TODO add printer selection activity.
+            }
+        }
     }
 }
