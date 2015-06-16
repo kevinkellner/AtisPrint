f33af17e78e5e3db8650d184eaee00978b1a9b08
kevin_kellner <kellnerkev@gmail.com>
2015-06-16 21:16:13 +0200

    fixed the issue with the loading of a resource file

--- src/test/java/edu.kit.uneig.atisprint.tests/SSHSessionTest.java
+++ src/test/java/edu.kit.uneig.atisprint.tests/SSHSessionTest.java
@@ -1,29 +1,35 @@
 package edu.kit.uneig.atisprint.tests;
 
+import android.content.res.Resources;
+import android.test.InstrumentationTestCase;
 import com.jcraft.jsch.JSchException;
 import edu.kit.uneig.atisprint.SSHInterface;
 import edu.kit.uneig.atisprint.SSHSession;
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 import java.util.Scanner;
 
 /**
  * Contains tests for the ssh session class.
  */
-public class SSHSessionTest {
+public class SSHSessionTest extends InstrumentationTestCase{
 
     private static SSHInterface session;
 
-    @BeforeClass
-    public static void setup() throws IOException {
+    @Before
+    public void setup() throws Exception {
+        super.setUp();
         Properties prop = new Properties();
-        prop.load(SSHSessionTest.class.getResourceAsStream("loginData"));
+        InputStream instream = getInstrumentation().getTargetContext().getResources().getAssets().open("loginData");
+        prop.load(instream);
         String username = prop.getProperty("username");
         String password = prop.getProperty("password");
         String hostname = prop.getProperty("hostname");
@@ -36,7 +42,6 @@ public class SSHSessionTest {
         String response = session.execute("dir");
         Assert.assertEquals("Response of dir command should not be null", response.length(), 0);
     }
-
 
 
 }
