a2ea5710deba30e8dd0bc1172241f734fcd4f04c
kevin_kellner <kellnerkev@gmail.com>
2015-06-25 12:37:08 +0200

    Added SSHJobs class to encapsulate some useful methods to be used with the SSHInterface

---
+++ src/main/java/edu/kit/uneig/atisprint/SSHJobs.java
@@ -0,0 +1,31 @@
+package edu.kit.uneig.atisprint;
+
+/**
+ * This class uses the SSHInterface to provide some methods useful for interaction over ssh.
+ * @author Kevin Kellner
+ * @version 1.0
+ */
+public class SSHJobs {
+    private SSHInterface ssh;
+    public SSHJobs(SSHInterface ssh) {
+        this.ssh = ssh;
+    }
+
+    public int countFiles(String dir) {
+        return getFileList(dir).length;
+    }
+
+    public String[] getFileList(String dir) {
+        //TODO: Need to change directory.
+        String fileList = ssh.execute("ls");
+        String files[] = fileList.split("\n");
+    }
+
+    public boolean deleteFiles(String dir){
+        //TODO dir may not be root dir
+        String[] files = getFileList(dir);
+        for (String file : files) {
+            ssh.execute("rm "+ dir + file); //Delete all files from directory
+        }
+    }
+}
